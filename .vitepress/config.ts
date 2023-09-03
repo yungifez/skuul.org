866d3a71a4f8cfcb6aa75b7e5b5085ddbac4151d
yungifez <ifezueyoung@gmail.com>
2023-09-03 00:43:13 +0000

    Fix broken styles

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Skuul.org",
-  base: "/repo/",
+  base: "/skuul.org/",
   description: "A multi school management system",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
